9b14adf1be2b3451a9ab1c9177cf2eb18a60fc5d
denisgarciaglobal <denis.garcia@global.com>
2021-08-19 17:24:38 +0100

    feat: add max-commit param

--- src/release_on_push_action/core.clj
+++ src/release_on_push_action/core.clj
@@ -1,17 +1,17 @@
 (ns release-on-push-action.core
-  (:require [babashka.curl :as curl]
-            [cheshire.core :as json]
-            [clojure.string :as str]
-            [clojure.java.io]
+    (:require [babashka.curl :as curl]
+              [cheshire.core :as json]
+              [clojure.string :as str]
+              [clojure.java.io]
 
-            [release-on-push-action.github :as github]))
+              [release-on-push-action.github :as github]))
 
 ;; -- Configuration Parsing  ---------------------------------------------------
 (defn getenv-or-throw [name]
   (let [val (System/getenv name)]
     (when (empty? val)
-      (throw (ex-info (str "Expected environment variable to be set: " name)
-                      {:env/name name})))
+          (throw (ex-info (str "Expected environment variable to be set: " name)
+                          {:env/name name})))
     val))
 
 (defn input-strategy-set? []
@@ -23,7 +23,7 @@
 
 (defn assert-valid-bump-version-scheme [bump-version-scheme]
   (when-not (contains? #{"major" "minor" "patch" "norelease"} bump-version-scheme)
-    (throw (ex-info (str "Invalid bump-version-scheme. Expected one of major|minor|patch|norelease. Got: " bump-version-scheme) {:bump-version-scheme bump-version-scheme})))
+            (throw (ex-info (str "Invalid bump-version-scheme. Expected one of major|minor|patch|norelease. Got: " bump-version-scheme) {:bump-version-scheme bump-version-scheme})))
   bump-version-scheme)
 
 (defn context-from-env
@@ -42,7 +42,9 @@
                              (if (input-strategy-set?)
                                (getenv-or-throw "INPUT_STRATEGY")
                                (throw ex)))))
-   :dry-run             (contains? (set args) "--dry-run")})
+   :dry-run             (contains? (set args) "--dry-run")
+   :input/max-commits   (System/getenv "INPUT_MAX_COMMITS")
+   })
 
 ;; -- Version Bumping Logic  ---------------------------------------------------
 (defn fetch-related-data [context]
@@ -56,10 +58,10 @@
 (defn bump-version-scheme [context related-data]
   (let [labels (get-labels (:related-prs related-data))]
     (cond
-      (contains? labels "release:major") :major
-      (contains? labels "release:minor") :minor
-      (contains? labels "release:patch") :patch
-      :else (keyword (:bump-version-scheme context)))))
+     (contains? labels "release:major") :major
+     (contains? labels "release:minor") :minor
+     (contains? labels "release:patch") :patch
+     :else (keyword (:bump-version-scheme context)))))
 
 (defn get-tagged-version [latest-release]
   (let [tag      (get latest-release :tag_name "0.0.0")
@@ -72,26 +74,26 @@
 (defn semver-bump [version bump]
   (let [[major minor patch] (map #(Integer/parseInt %) (str/split version #"\."))
         next-version (condp = bump
-                       :major [(safe-inc major) 0 0]
-                       :minor [major (safe-inc minor) 0]
-                       :patch [major minor (safe-inc patch)])]
+                            :major [(safe-inc major) 0 0]
+                            :minor [major (safe-inc minor) 0]
+                            :patch [major minor (safe-inc patch)])]
     (str/join "." next-version)))
 
 (defn norelease-reason [context related-data]
   (cond
-    (= :norelease (bump-version-scheme context related-data))
-    "Skipping release, no version bump found."
+   (= :norelease (bump-version-scheme context related-data))
+   "Skipping release, no version bump found."
 
-    (str/includes? (github/commit-title (:commit related-data)) "[norelease]")
-    "Skipping release. Reason: git commit title contains [norelease]"
+   (str/includes? (github/commit-title (:commit related-data)) "[norelease]")
+   "Skipping release. Reason: git commit title contains [norelease]"
 
-    (contains? (get-labels (get-in related-data [:related-prs])) "norelease")
-    "Skipping release. Reason: related PR has label norelease"))
+   (contains? (get-labels (get-in related-data [:related-prs])) "norelease")
+   "Skipping release. Reason: related PR has label norelease"))
 
 ;; Note: at 500, I suspect this list will be unproductive to view
-(def max-commits-to-summarize
+(defn max-commits-to-summarize [max-commits]
   "This is the maximum number of commits to summarize."
-  50)
+  max-commits)
 
 (defn generate-new-release-data [context related-data]
   (let [bump-version-scheme (bump-version-scheme context related-data)
@@ -100,18 +102,18 @@
         base-commit         (get-in related-data [:latest-release :target_commitish])
 
         summary-since-last-release (->> (github/list-commits-to-base context base-commit)
-                                        (take max-commits-to-summarize)
+                                        (take (max-commits-to-summarize (Integer. (:input/max-commits context))))
                                         (map github/commit-summary)
                                         (str/join "\n"))]
     {:tag_name         (str (:input/tag-prefix context) next-version)
      :target_commitish (:sha context)
      :name             (str (:input/tag-prefix context) next-version)
      :body             (with-out-str
-                         (printf "Version %s\n\n" next-version)
-                         (when-let [body (:input/release-body context)]
-                           (println body))
-                         (printf "### Commits\n\n")
-                         (println summary-since-last-release))
+                        (printf "Version %s\n\n" next-version)
+                        (when-let [body (:input/release-body context)]
+                          (println body))
+                        (printf "### Commits\n\n")
+                        (println summary-since-last-release))
      :draft            false
      :prerelease       false}))
 
